9e714a73ccd9410804c5e4f3c53b06fca6c1b843
Kedap <kedap.dev@protonmail.com>
2022-12-28 22:12:32 -0600

    se pueden registrar nuevos usuarios

---
+++ registrer.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="es">
+  <head>
+    <title>Registrarse</title>
+    <meta charset="UTF-8" />
+  </head>
+  <body>
+    <h1>Registrar nuevo usuario</h1>
+    <?php
+    if (!empty($_POST['usuario']) && !empty($_POST['password'])) {
+        include "./php/db/usuarios.php";
+        registrar_usuario($_POST['usuario'], $_POST['password']);
+        echo "Se creo el usuario correctamente";
+    }
+    ?>
+    <form action="registrer.php" method="post">
+      Usuario: <input type="text" name="usuario" /> <br />
+      Contraseña: <input type="password" name="password" /> <br />
+      <input type="submit" value="Registrar nuevo usuario" />
+    </form>
+  </body>
+</html>
